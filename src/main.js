8ea2f4e914ba96900b8958b40c4180df86f7d3cf
jamond-x <x2592253600@163.com>
2021-06-23 17:12:21 +0800

    fix&feat: sass配置，解决eslint报错问题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/axios'
+//import App from './App.vue'
+import App from './layout/App.vue'
+import router from './router'
+import store from './store'
+import './quasar'
+import axios from 'axios'
+
+
+//Vue.prototype.$axios = axios    //全局注册，使用方法为:this.$axios
+//Vue.prototype.qs = qs           //全局注册，使用方法为:this.qs
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  axios,
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-page>
+    <div class="row justify-center full items-center">
+      <div class="col-xs-8 col-sm-6 col-md-5 col-lg-4">
+        <q-tabs
+        v-model="tab"
+        dense
+        align="justify"
+        class="bg-primary text-white shadow-2"
+        :breakpoint="0"
+      >
+        <q-tab name="mails" icon="mail" />
+        <q-tab name="alarms" icon="alarm" />
+      </q-tabs>
+      </div>
+      
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data: function() {
+    return {
+      result: '回应',
+      tab: '选项'
+    }
+  },
+  methods:{
+   
+      
+  }
+}
+
+
+
+
+//  async go(){
+//       await this.$axios.post('https://qcdhrv.fn.thelarkcloud.com/hello',{user:'xxq',password:'259'})
+//       .then(
+//         res => {
+//           this.result = res.data
+//           console.log(res.data);
+//         }
+//       )
+
+//       await new Promise((resolve) => {
+//         console.log(1);
+//         resolve()
+        
+//       }).then(
+//         () => {
+//           console.log(2);
+//         }
+//       ).catch(
+//         () => {
+//           console.log('failed');
+//         }
+//       )
+
+//       console.log('done');
+//     }
+
+</script>
+
+
+<style lang="scss" scoped>
+h1{
+  color:$main-color
+}
+</style>
